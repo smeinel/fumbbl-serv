acdaa31a80dd3fca6d5cdd04f72dd0908c29b9f5
Steve Meinel <steve@stevemeinel.com>
2015-04-11 00:51:03 -0700

    exploratory change of XML parsers

--- routes/matches.js
+++ routes/matches.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var HTTP = require('q-io/http');
+var Q = require('q');
+var parseString = Q.denodeify(require('xml2js').parseString);
+var parser = require('xml2json');
 
 var config = require('../config');
 
 router.get('/', function (req, res) {
 	var remote_request = HTTP.read(config.FUMBBL_URL + 'matches');
 	console.log('requesting ' + config.FUMBBL_URL + 'matches');
-	remote_request.then(function (body) {
-		res.header('Content-Type','text/xml');
+	remote_request.then(function (xml_body) {
+		// return parseString(xml_body, {mergeAttrs: true});
+		return parser.toJson(xml_body);
+	})
+	.then(function (body) {
+		res.header('Content-Type','application/json');
 		res.send(body);
 	}).fail(function (err) {
 		console.error('ERROR', err);
