4f724e0af18877a1696485fbbf6b9f9a1a0ea595
Steve Meinel <steve@stevemeinel.com>
2015-04-11 00:06:47 -0700

    First proxy of FUMBBL XML

--- routes/matches.js
+++ routes/matches.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var HTTP = require('q-io/http');
 
 var config = require('../config');
 
 router.get('/', function (req, res) {
+	var remote_request = HTTP.read(config.FUMBBL_URL + 'matches');
 	console.log('requesting ' + config.FUMBBL_URL + 'matches');
-	res.send('/matches/');
+	remote_request.then(function (body) {
+		res.header('Content-Type','text/xml');
+		res.send(body);
+	}).fail(function (err) {
+		console.error('ERROR', err);
+	});
 });
 
 module.exports = router;
